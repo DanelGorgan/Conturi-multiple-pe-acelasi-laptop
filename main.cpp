77f52c227546079337ca7a1a7b3d76d923b1cbe9
DanelGorgan <danielgorgan24@yahoo.com>
2016-12-28 20:18:36 +0200

    Dani
    Dani

--- main.cpp
+++ main.cpp
@@ -1,52 +1,175 @@
 #include <iostream>
-#define tipDate int
 
 using namespace std;
 
 struct nod
 {
     int info;
-    nod *urm;
+    nod* stg;
+    nod* dr;
 };
 
-nod *L;
-
-
-void citesteListaSimpla(nod *&L)
+void adaugare(nod *&a, int el)
 {
-    nod *p;
-    unsigned int nrElemente;
-    cout<<"Dati numarul de elemente: ";
-    cin>>nrElemente;
-
-    tipDate element;
-    L=NULL;
-    for (unsigned int i=1; i<=nrElemente; i++)
+    if (a==NULL)
     {
-        cout<<"Dati elementul al "<<i<<"-lea: ";
-        cin>>element;
-        p=new nod;
-        p->info=element;
-        p->urm=L;
-        L=p;
+        a=new nod;
+        a->info=el;
+        a->stg=NULL;
+        a->dr=NULL;
+    }
+    else if (el<=a->info)
+        adaugare(a->stg,el);
+    else adaugare(a->dr,el);
+}
+
+void creare (nod*&a)
+{
+    unsigned int i;
+    unsigned int n;
+    int x;
+    a=NULL;
+    cout<<"Dati nr de elemente: ";
+    cin>>n;
+    for (i=1; i<=n; i++)
+    {
+        cout<<"Dati elementul al "<<i<<" lea: ";
+        cin>>x;
+        adaugare(a,x);
     }
 }
 
-void afiseazaListaSimpla(nod* L)
+bool cautare(nod *a,int el)
 {
-    nod *p;
-    cout<<'\n';
-    p=L;
-    while (p!=NULL)
+    if (a==NULL) return false;
+    else if (a->info==el)
+        return true;
+    else if (el<a->info) return cautare(a->stg,el);
+    return cautare(a->dr,el);
+}
+
+void postordine (nod*a)
+{
+    if (a)
     {
-        cout<<p->info<<" ";
-        p=p->urm;
+        postordine(a->stg);
+        postordine(a->dr);
+        cout<<a->info<<" ";
+    }
+}
+void inordine (nod*a)
+{
+    if (a)
+    {
+        inordine(a->stg);
+        cout<<a->info<<" ";
+        inordine(a->dr);
+    }
+}
+
+void preordine (nod*a)
+{
+    if (a)
+    {
+        cout<<a->info<<" ";
+        preordine(a->stg);
+        preordine(a->dr);
+    }
+}
+int inaltime (nod*a)
+{
+    if (a==NULL)
+        return -1;
+    else
+    {
+        int h1=inaltime(a->stg);
+        int h2=inaltime(a->dr);
+        return 1+(h1>h2?h1:h2);
+    }
+}
+
+int CoadaVida()
+{
+    return NULL;
+}
+
+void parcurgereLatime(nod *a)
+{
+    nod *Q[100];
+    int prim=0,ultim=0;
+    if (a==NULL) return;
+    else
+    {
+        Q[prim]=a;
+        while (prim<=ultim)
+        {
+            cout<<Q[prim]->info<<" ";
+            if (Q[prim]->stg!=NULL)
+            {
+                Q[++ultim]=Q[prim]->stg;
+            }
+            if (Q[prim]->dr!=NULL)
+            {
+                Q[++ultim]=Q[prim]->dr;
+            }
+            prim++;
+        }
+    }
+}
+
+void parcurgereAdancime(nod *a)
+{
+    nod *Q[100];
+    int prim=0,ultim=0;
+    if (a==NULL) return;
+    else
+    {
+        Q[prim]=a;
+        while (prim<=ultim)
+        {
+            cout<<Q[prim]->info<<" ";
+            if (Q[prim]->stg!=NULL)
+            {
+                Q[++ultim]=Q[prim]->stg;
+            }
+            else
+            {
+                if (Q[prim]->dr!=NULL)
+                {
+                    Q[++ultim]=Q[prim]->dr;
+                }
+
+
+            }
+            prim++;
+        }
     }
 }
 int main()
 {
-    citesteListaSimpla(L);
+    nod* a;
+    int x;
+    creare(a);
+    cout<<" \n Preordine: ";
+    preordine(a);
+    cout<<" \n Inordine: ";
+    inordine(a);
+    cout<<" \n Postordine: ";
+    postordine(a);
+    cout<<" \n In latime: ";
+    parcurgereLatime (a);
+    cout<<" \n In adancime: ";
+    parcurgereAdancime (a);
+    do
+    {
+        cout<<"\n Dati elementul cautat: ";
+        cin>>x;
+        if (cautare(a,x))
+            cout<<x<<" se gaseste \n";
+        else
+            cout<<" nu se gaseste \n";
+    }
+    while (x!=0);
 
-    afiseazaListaSimpla(L);
     return 0;
 }
